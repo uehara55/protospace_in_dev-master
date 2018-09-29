8c7fea5d42f667a0535f365bc256e5fd3649ba0f
uehara55 <yusukeuehara55@gmail.com>
2018-09-29 18:26:31 +0900

    Modified

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Miuraspace
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
 
     config.generators do |g|
       g.test_framework       false
@@ -19,4 +20,3 @@ module Miuraspace
   end
 end
 
-config.assets.initialize_on_precompile = false
